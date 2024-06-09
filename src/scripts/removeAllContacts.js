f4d2fdf739af3b75c6eb8b4e5a81bc2bd3539f8f
Alina Alieks <alieksieienko.alina@gmail.com>
2024-06-09 18:06:54 +0300

    add all func

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  const data = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+  JSON.stringify(await fs.writeFile(PATH_DB, JSON.stringify([])));
+  console.log(data);
+  return data;
+};
 
 await removeAllContacts();
--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { getAllContacts } from './getAllContacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  const allContacts = await getAllContacts();
+  const halfContacts = allContacts.filter(() => Math.random() >= 0.5);
+  await fs.writeFile(PATH_DB, JSON.stringify(halfContacts));
+  //   console.log(halfContacts);
+  return halfContacts;
+};
 
 await thanos();
